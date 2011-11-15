7e7f8cf701f7e190357b3ce143d6e7f655375d02
alex <alex@08fa6d5c-9368-0410-8c78-aaaba8fdd15f>
2011-11-15 20:43:42 +0000

    TWMain now includes arc4random_uniform for < 4.3 compatibility

--- trunk/source-ARC/application/TWMain.m
+++ trunk/source-ARC/application/TWMain.m
@@ -13,11 +13,21 @@ static void uncaughtExceptionHandler(NSException *exception)
 
 int main(int argc, char *argv[])
 {
-   // note that using PLCrashReporter or such will most likely override this
+   // note that using PLCrashReporter or such will most likely override this;
+   // TestFlight alleges that it will call a handler installed before -takeOff
    NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
+   // and it would also call existing signal handlers, did we install any here
    
    @autoreleasepool
    {
       return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
    }
 }
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_4_3
+// declared in stdlib.h
+u_int32_t arc4random_uniform(u_int32_t upper_bound)
+{
+   return arc4random() % upper_bound;
+}
+#endif // __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_4_3
